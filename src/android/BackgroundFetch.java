c654da657eaa9c1fc49e264761a1ecfa233911b7
Sung Won Cho <mikeswcho@gmail.com>
2015-11-19 14:39:31 +1100

    Increase interval to 30 minutes to avoid draining battery

--- src/android/BackgroundFetch.java
+++ src/android/BackgroundFetch.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -13,11 +14,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class BackgroundFetch extends CordovaPlugin {
-    public static final int PERIOD = 900000; // Every 15 minutes
+    public static final int PERIOD = 1800000; // Every 30 minutes
     public static final String ACTION_RUN_BACKGROUND_FETCH = "ED_RUN_BACKGROUND_FETCH";
 
     @Override
     public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d("BackgroundFetch", "Running backgroundFetch");
         if (action.equals("configure")) {
             Context appContext = cordova.getActivity().getApplicationContext();
             AlarmManager alarmMgr = (AlarmManager) appContext.getSystemService(Context.ALARM_SERVICE);
@@ -28,7 +30,7 @@ public class BackgroundFetch extends CordovaPlugin {
             PendingIntent pi = PendingIntent.getBroadcast(appContext, 0, i ,0);
 
             alarmMgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    SystemClock.elapsedRealtime(),
+                    SystemClock.elapsedRealtime() + 60000,
                     PERIOD,
                     pi);
 
